88b113d84adc16758eb63edb539acf3be7559a17
Anmol Kansal <anmolkansal1805+github@gmail.com>
2025-04-03 16:37:43 +0530

    Update celery_factory.py

--- celery_factory.py
+++ celery_factory.py
@@ -1,4 +1,4 @@
-from celery import Celery, Task
+'''from celery import Celery, Task
 from flask import Flask
 from app import app
 
@@ -22,6 +22,30 @@ def celery_init_app(app: Flask) -> Celery:
 
     celery_app.set_default()
     app.extensions["celery"] = celery_app
+    return celery_app'''
+
+import os
+from celery import Celery, Task
+from flask import Flask
+
+class CeleryConfig:
+    broker_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+    result_backend = os.getenv("REDIS_URL", "redis://localhost:6379/1")
+    timezone = 'Asia/Kolkata'
+
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(CeleryConfig)
+
+    celery_app.autodiscover_tasks(['tasks'], force=True)  # Ensure your tasks are in a 'tasks' module
+
+    app.extensions["celery"] = celery_app
     return celery_app
+
 
 
